739e47badafe327f902d784e2e8c509c9ddd9602
Samsung <Samsung@DESKTOP-KGE5MCR>
2021-03-06 11:08:17 -0600

    version1.2

---
+++ src/gui/GUI.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Samsung
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame ();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setTitle("Cesba Ventana");
+        
+        frame.setResizable(true);
+        //formato default
+        //frame.getContentPane().setBackground(Color.GREEN);
+        //formato RGB
+        //frame.getContentPane().setBackground(new Color (28,26, 198));
+        //FORMATO HEXADECIMAL:
+        frame.getContentPane().setBackground (new Color (0x1AC3C6));
+        
+     // TODO code application logic here
+    }
+    
+}
